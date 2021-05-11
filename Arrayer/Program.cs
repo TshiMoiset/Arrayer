553c886ff1962eb86adc6566799aaa0e65af4fd3
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-11 21:21:43 +0200

    Planets

---
+++ Arrayer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            //string[] planets = { "Proxima Centauri B", "Proxima Centauri C", "Kepler-1229b", "Kepler-442b", "Kepler-186f" };
+            string[] planets = { "KEPLER-438B", "KEPLER-452B", "PROXIMA-B", "KEPLER-186F", "KEPLER-62F", "GLISE-667CC", "KEPLER-62E", "GLIESE-581G", "KEPLER-22B" };
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Hej och välkomen ombord på Novax (Nova Explore).");
+            Console.WriteLine("Tryck [ENTER] för att se resealternativ");
+
+            answer = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < planets.Length; i++)
+            {
+                Console.WriteLine($"{planets}");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
