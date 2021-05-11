026144c382d47c200d47cb75d46b53015f6b29e0
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-11 23:23:18 +0200

    Update Program.cs

--- Arrayer/Program.cs
+++ Arrayer/Program.cs
@@ -7,7 +7,7 @@ namespace Arrayer
         static void Main(string[] args)
         {
             int planetChoice = 0;
-            string answer = "";
+
             //string[] planets = { "Proxima Centauri B", "Proxima Centauri C", "Kepler-1229b", "Kepler-442b", "Kepler-186f" };
             string[] planets = { "KEPLER-438B", "KEPLER-452B", "Proxima Centauri B", "KEPLER-186F", "Proxima Centauri C", "GLISE-667CC", "KEPLER-62E", "GLIESE-581G", "KEPLER-22B" };
 
