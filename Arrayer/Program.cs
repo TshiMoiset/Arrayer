52e388f51fc3256ddb501afe9735fa6aefed5601
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-12 11:50:56 +0200

    Update Program.cs

--- Arrayer/Program.cs
+++ Arrayer/Program.cs
@@ -44,49 +44,96 @@ namespace Arrayer
 
             Console.ForegroundColor = ConsoleColor.Yellow;
 
-            /*switch ()
+            Random random = new Random();
+            /*Random daysToTravell = new Random();
+            Random yearsToTravell = new Random();
+            Random monthsToTravell = new Random();*/
+
+            int years = 0;
+            int days = 0;
+            int months = 0;
+
+            switch (planetChoice)
             {
-                case "1":
+                case 1:
+                    years = random.Next(400, 500);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[0]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "2":
+                case 2:
+                    years = random.Next(1400, 1500);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[1]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "3":
+                case 3:
+                    years = random.Next(4, 10);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[2]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "4":
+                case 4:
+                    years = random.Next(500, 600);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[3]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "5":
+                case 5:
+                    years = random.Next(4, 10);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[4]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "6":
+                case 6:
+                    years = random.Next(20, 30);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[5]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "7":
+                case 7:
+                    years = random.Next(1200, 1300);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[6]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "8":
+                case 8:
+                    years = random.Next(20, 30);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[7]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
 
-                case "9":
+                case 9:
+                    years = random.Next(500, 600);
+                    days = random.Next(1, 365);
+                    months = random.Next(1, 12);
                     Console.WriteLine($"Du har valt att åka till {planets[8]}");
+                    Console.WriteLine($"Det kommer att ta {years} år {days} dagar {months} månader");
                     break;
                 default:
-                    Console.WriteLine("ERROR!!!", ConsoleColor.Red);
-                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR!!!");
+                    Console.WriteLine("Du har inte valt en destination.");
+                    Console.WriteLine("Välj ett alternatuv mellan [1 - 9]");
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     break;
-            }*/
+            }
 
             Console.ReadLine();
 
